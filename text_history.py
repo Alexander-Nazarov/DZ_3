291da817935b64ff92a14c547748df2a3deac77a
Alexander-Nazarov <Alexnazavr@yandex.ru>
2018-10-20 21:45:56 +0300

    Update text_history.py

--- text_history.py
+++ text_history.py
@@ -2,37 +2,53 @@
 
 class TextHistory:
     def __init__(self):
-        self.text = 'abc'
+        self.text = ''
         self.version = int(0)
-    def insert(self, tekst, pos = None):
-        if pos is None:
-            pos = len(self.text)
-        self.insert = InsertAction.insert(self, self.text, tekst, pos)
+        self.history = []
+    def apply_action(self, step):
+        self.text = step.apply_action(self.text)
+        self.history.append(step)
+        self.version += 1
+        return self.version
+
+    def insert(self, text, pos):
+        step = InsertAction(text, pos, self.version, self.version + 1)
+        return self.apply_action(step)
+
+
+    def replace(self, text, pos):
+        pass
+    def delete(self, pos, length):
+        pass
+    def action(self, action):
+        pass
+    def get_action(self, from_version, to_version):
+        pass
+
 
 class Action:
     def __init_(self, from_version, to_version):
         self.from_version = from_version
         self.to_version = to_version
-    def apply(stroka, action):
-        #if action is InsertAction:
-
-        return new_stroka
 
 
 class InsertAction(Action):
-    def insert(self ,text, tekst, pos):
-        #print(text)
-        if len(text) >= pos >= 0:
-            BEGIN = text[0:int(pos)]
-            #print(tekst)
-            END = text[int(pos):]
-            text = BEGIN + tekst + END
-            #print(text)
-            return #self.version + 1
+    def __init__(self, text, pos, from_version, to_version):
+        if text != None:
+            self.text = text
+        if pos < 0 or pos is None:
+            raise ValueError()
         else:
-            raise ValueError
-        return
+            self.pos = pos
 
+    def apply_action(self, text_from_history):
+        if len(text_from_history) < self.pos:
+            raise ValueError()
+        else:
+            BEGIN = text_from_history[:self.pos]
+            END = text_from_history[self.pos:]
+            text_from_history = BEGIN + self.text + END
+        return text_from_history
 
 class ReplaceAction(Action):
     pass
@@ -41,6 +57,9 @@ class ReplaceAction(Action):
 class DeleteAction(Action):
     pass
 
-h = TextHistory()
-#print(h.text, h.version)
-print(h.insert('xyz', 1))
+'''h = TextHistory()
+print(h.text)
+print(h.insert('abc', 0))
+print(h.text)
+print(h.insert('xyz', 2))
+print(h.text) '''
